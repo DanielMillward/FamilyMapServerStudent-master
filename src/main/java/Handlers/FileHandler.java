26df6bf49c94dd5bdcd5ed15da277cca71171801
danielfm <dfmillward02@gmail.com>
2022-02-21 14:58:20 -0700

    add file handler
    Now hosting website on the server itself

--- src/main/java/Handlers/FileHandler.java
+++ src/main/java/Handlers/FileHandler.java
@@ -3,11 +3,58 @@ package Handlers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.nio.file.Files;
 
 public class FileHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-
+        boolean success = false;
+        try {
+            // Make sure request method is GET
+            if (exchange.getRequestMethod().toLowerCase().equals("get")) {
+                String urlPathTemp = exchange.getRequestURI().toString();
+                String urlPath = null;
+                System.out.println("urlPathTemp is " + urlPathTemp);
+                if (urlPathTemp == null || urlPathTemp.equals("/")){
+                    urlPath = "/index.html";
+                } else {
+                    urlPath = urlPathTemp;
+                }
+                System.out.println("urlPath is " + urlPath);
+                //get file path
+                String filePath = "web" + urlPath;
+                System.out.println("requested " + filePath);
+                File requestedFile = new File(filePath);
+                OutputStream respBody = exchange.getResponseBody();
+                if (requestedFile.exists()) {
+                    System.out.println("File exists");
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+                    Files.copy(requestedFile.toPath(), respBody);
+                    respBody.close();
+                } else {
+                    File notFoundFile = new File("web/HTML/404.html");
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+                    Files.copy(notFoundFile.toPath(), respBody);
+                    respBody.close();
+                }
+                success = true;
+            }
+            if (!success) {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+                // close response to say we're done
+                System.out.println("Bad request...");
+                exchange.getResponseBody().close();
+            }
+        } catch (IOException e) {
+            // there was a server error
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_SERVER_ERROR, 0);
+            // close response to say we're done
+            exchange.getResponseBody().close();
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/Handlers/LoginHandler.java
+++ src/main/java/Handlers/LoginHandler.java
@@ -21,12 +21,13 @@ public class LoginHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-
-
+        System.out.println("got a login request!!!");
+        System.out.println(exchange.getRequestBody());
         boolean success = false;
         //rewrite comments from here
         try {
             // Make sure request method is POST
+            System.out.println(exchange.getRequestMethod());
             if (exchange.getRequestMethod().toLowerCase().equals("post")) {
                 // Get header
                 Headers reqHeaders = exchange.getRequestHeaders();
@@ -43,20 +44,25 @@ public class LoginHandler implements HttpHandler {
                 //send off loginRequest to the service
                 LoginService service = new LoginService();
                 LoginResult result = service.login(request);
+                System.out.println(result);
                 //Supposedly everything went well, so send a code 200
                 exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
                 // make a writer so we can make the response body
                 Writer resBody= new OutputStreamWriter(exchange.getResponseBody());
+                //TODO: actually send stuff to the resBody?
                 //send json result off to the writer which writes our HTTP response
                 gson.toJson(result, resBody);
                 //say we're done
                 resBody.close();
 
                 success = true;
+            } else if (exchange.getRequestMethod().toLowerCase().equals("options")) {
+                //Do something?
             }
             if (!success) { // The HTTP request was invalid somehow, so we return a "bad request"
                 exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
                 // close response to say we're done
+                System.out.println("Bad request...");
                 exchange.getResponseBody().close();
             }
         }
